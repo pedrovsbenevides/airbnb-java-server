7330a378c2dc9fbc63985106dc07190a2bfad196
Pedro-Benevides <pvsbenevides197@gmail.com>
2023-09-16 01:26:15 -0300

    feat(UserRepository): implementa consulta de usuario por uuid

--- src/main/java/com/airbnbserver/domain/repositories/UserRepository.java
+++ src/main/java/com/airbnbserver/domain/repositories/UserRepository.java
@@ -1,9 +1,13 @@
 package com.airbnbserver.domain.repositories;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.airbnbserver.domain.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    public User findUserByUuid(UUID userUuid);
 
 }
