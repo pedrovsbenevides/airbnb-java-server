e50cb74db5b39a1fe7114737d0ab017be30b247e
Pedro-Benevides <pvsbenevides197@gmail.com>
2023-09-19 00:25:37 -0300

    chore(AccommodationRepository): adiciona tipagem optional em consulta de acomodações de um host

--- src/main/java/com/airbnbserver/domain/repositories/AccommodationRepository.java
+++ src/main/java/com/airbnbserver/domain/repositories/AccommodationRepository.java
@@ -1,6 +1,7 @@
 package com.airbnbserver.domain.repositories;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,5 +10,5 @@ import com.airbnbserver.domain.entities.User;
 
 public interface AccommodationRepository extends JpaRepository<Accommodation, Long> {
 
-    public List<Accommodation> findAccommodationByHost(User host);
+    public Optional<List<Accommodation>> findAccommodationByHost(User host);
 }
