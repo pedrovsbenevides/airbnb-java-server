0aa6a12276ca67d07149d161977846413ea62d30
Pedro-Benevides <pvsbenevides197@gmail.com>
2023-09-19 00:23:24 -0300

    fix(migration): ajusta versionamento do banco

--- src/main/resources/db/migration/V2__create_accomodation_table.sql
+++ src/main/resources/db/migration/V2__create_accomodation_table.sql
@@ -1,11 +1,11 @@
-CREATE TABLE acomodacao (
+CREATE TABLE accommodation (
     id SERIAL PRIMARY KEY,
     uuid UUID NOT NULL,
     user_id SERIAL NOT NULL,
     cep VARCHAR(8) NOT NULL,
     address VARCHAR(255) NOT NULL,
     complement VARCHAR(255) NULL DEFAULT NULL,
-    daily_fee FLOAT(8, 2) NOT NULL,
+    daily_fee DECIMAL(8, 2) NOT NULL,
     description VARCHAR(255) NULL DEFAULT NULL,
-    capacity VARCHAR(45) NOT NULL,
+    capacity VARCHAR(45) NOT NULL
 );
