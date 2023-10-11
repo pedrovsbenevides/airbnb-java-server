354612901de2ecac99e50fd5a57fa9c962eaaacc
Pedro-Benevides <pvsbenevides197@gmail.com>
2023-10-10 23:48:52 -0300

    feat(UserRepository): implementa métodos de checagem de existencia e ajusta retorno de consulta por uuid

--- src/main/java/com/airbnbserver/domain/repositories/UserRepository.java
+++ src/main/java/com/airbnbserver/domain/repositories/UserRepository.java
@@ -1,5 +1,6 @@
 package com.airbnbserver.domain.repositories;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,6 +9,10 @@ import com.airbnbserver.domain.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    public User findUserByUuid(UUID userUuid);
+    public Optional<User> findUserByUuid(UUID uuid);
+
+    public boolean existsUserByEmail(String email);
+
+    public boolean existsUserByCpf(String cpf);
 
 }
